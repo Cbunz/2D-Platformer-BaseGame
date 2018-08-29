52c65f3a9362524d3bbb36fc93f050166e041270
Cbunz <anderson.connor.d@gmail.com>
2018-08-28 18:06:54 -0700

    First Commit
    Created new repository for a base game of a 2D platformer.

---
+++ 2D Platformer Base Game/Assets/Scripts/StateMachineBehavior/AirMeleeAttackSMB.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirMeleeAttackSMB : SceneLinkedSMB<PlayerCharacter>
+{
+    public override void OnSLStatePostEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        monoBehaviour.EnableMeleeAttack();
+        if (monoBehaviour.dashWhileAirborne)
+        {
+            monoBehaviour.SetHorizontalMovement(monoBehaviour.meleeAttackDashSpeed * monoBehaviour.GetFacing());
+        }
+    }
+
+    public override void OnSLStateNoTransitionUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("Air Melee State");
+        monoBehaviour.UpdateJump();
+        monoBehaviour.AirHorizontalMovement();
+        monoBehaviour.AirVerticalMovement();
+        monoBehaviour.CheckOnGround();
+    }
+
+    public override void OnSLStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        monoBehaviour.DisableMeleeAttack();
+    }
+}
---
+++ 2D Platformer Base Game/Assets/Scripts/StateMachineBehavior/TakeDamageSMB.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeDamageSMB : SceneLinkedSMB<PlayerCharacter>
+{
+    public override void OnSLStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        monoBehaviour.SetMoveVector(monoBehaviour.GetHurtDirection() * monoBehaviour.hurtJumpSpeed);
+        monoBehaviour.StartFlickering();
+    }
+
+    public override void OnSLStateNoTransitionUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("Take Damage State");
+        if (monoBehaviour.IsFalling())
+        {
+            monoBehaviour.CheckOnGround();
+        }
+        monoBehaviour.AirVerticalMovement();
+    }
+}
